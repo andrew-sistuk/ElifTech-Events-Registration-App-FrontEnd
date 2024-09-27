36c51171859d7c3340ff00f44871e941b49b9725
Andrew Sistuk <32619379+andrew-sistuk@users.noreply.github.com>
2024-09-27 18:58:30 +0300

    Add loading spinner to Loader component and create NotFound page
    - Include `react-loader-spinner` dependency for the loading animation.
    - Implement `MagnifyingGlass` spinner in the `Loader` component.
    - Create `NotFound` component with 404 message and styling.
    - Add new CSS styles for the `NotFound` component layout and text appearance.

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,19 @@
 import css from './Loader.module.css';
+import { MagnifyingGlass } from 'react-loader-spinner';
 
 export default function Loader() {
   return (
     <div className={css.backdrop}>
       <div className={css.modal}>
-        <div className={css.box}>
-          <div className={css.mask}>
-            <span className="material-symbols-outlined">water_bottle_large</span>
-          </div>
-        </div>
+        <MagnifyingGlass
+          visible={true}
+          ariaLabel="magnifying-glass-loading"
+          height="150"
+          width="150"
+          wrapperClass="magnifying-glass-wrapper"
+          glassColor="#cff09e"
+          color="#507c5c"
+        />
       </div>
     </div>
   );
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,13 @@
+import clsx from 'clsx';
+import css from './NotFound.module.css';
+
 function NotFound() {
-  return <div>test</div>;
+  return (
+    <div className={clsx(css['text-container'], 'container')}>
+      <p className={css.text}>404</p>
+      <p className={css.text}>Page not found.</p>
+    </div>
+  );
 }
 
 export default NotFound;
